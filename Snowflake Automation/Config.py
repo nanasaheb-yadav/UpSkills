c15317fa393f76ef228bf74e91a68aca7839e4f6
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-11-23 00:26:00 +0530

    Updating codes

--- Snowflake Automation/Config.py
+++ Snowflake Automation/Config.py
@@ -1,6 +1,10 @@
 USERNAME = ""
 PASSWORD = ""
-ACCOUNT = ""
-WAREHOUSE = ""
-SCHEMA = ""
+USERROLE = ""
+ACCOUNT = "xxxxx.us-xxxx-1"
+WAREHOUSE = "SF_AUTOMATION"
+DATABASE = "SF_Auto"
+SCHEMA = "PUBLIC"
 TABLES = ""
+
+SRCFILE = ""
