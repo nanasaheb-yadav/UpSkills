8c28f30b4941e09f4308ffcbfe309764ef34c32b
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-11-24 00:02:36 +0530

    adding new files for Teradata Automation Directory

---
+++ Teradata Automation/TeradataConnection.py
@@ -0,0 +1,12 @@
+try:
+    import teradata as td
+except ImportError as err:
+    print(f"Error importing; {err}")
+    exit(1)
+
+class TeradataConnection:
+
+    def __init__(self):
+        pass
+
+    
