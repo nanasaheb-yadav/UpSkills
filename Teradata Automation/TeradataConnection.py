7f4c0e23b94ca1bef8d0f02ac1cbcd4dbe8f377d
Nanasaheb Yadav <nanayadav33@gmail.com>
2021-11-24 00:14:28 +0530

    updating files for Teradata Automation Directory

--- Teradata Automation/TeradataConnection.py
+++ Teradata Automation/TeradataConnection.py
@@ -1,12 +1,27 @@
 try:
-    import teradata as td
+    import teradatasql as td
+    from Config import SERVER, USERNAME, PASSWORD
 except ImportError as err:
     print(f"Error importing; {err}")
     exit(1)
+
 
 class TeradataConnection:
 
     def __init__(self):
         pass
 
-    
+    def td_connection(self, host=SERVER, username=USERNAME, password=PASSWORD):
+        """
+        Connect to Teradata Server for given host details.
+        :param host:
+        :param username:
+        :param password:
+        :return: connection object: connection
+        """
+        try:
+            conn = td.connect(host, username, password)
+            return conn
+        except Exception as err:
+            print(f"Error connecting to Teradata {err}")
+            exit(1)
